36d14d178001bfe1c2c3832a854d545554927f4f
Igal Tabachnik <hmemcpy@gmail.com>
2016-06-15 00:04:08 +0300

    first commit

---
+++ PerfDemos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfDemos
+{
+    class Program
+    {
+        static void Main()
+        {
+            Boxing.Demo();
+            Enum.Demo(ConsoleModifiers.Control | ConsoleModifiers.Shift);
+        }
+    }
+}
